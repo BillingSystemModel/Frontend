e8f05f47c019b805b448433f613f8c2ff374330c
Vsevolod Romashov <avs.rom@gmail.com>
2023-12-03 22:36:09 +0300

    Fix #9: Создано навигациоонное меню

--- src/app/components/Layout/Layout.tsx
+++ src/app/components/Layout/Layout.tsx
@@ -1,12 +1,15 @@
 import {memo} from 'react';
 import {Outlet} from 'react-router-dom';
+import {SideMenu} from '../SideMenu/SideMenu';
+import './Layout.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 export const Layout = memo(function Layout() {
     // Наш App.tsx
     return (
-        <div>
-            {/*<SideMenu />*/}
-            Hello!
+        <div className="layout-container">
+            <SideMenu />
             <Outlet />
         </div>
     );
