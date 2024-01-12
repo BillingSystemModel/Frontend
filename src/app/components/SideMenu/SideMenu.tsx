62eb298a1b5ba5b51388ba13f693fbb55e820927
ДшLidiya Klochkova <lida.klochkova.02@mail.ru>
2024-01-13 00:06:46 +0300

    Fix personal account routing in side menu

--- src/app/components/SideMenu/SideMenu.tsx
+++ src/app/components/SideMenu/SideMenu.tsx
@@ -33,7 +33,7 @@ export const SideMenu = memo(function SideMenu() {
             <CDBSidebarContent>
                 <CDBSidebarMenu>
                     <CDBSidebarMenuItem className="side-menu-item" icon="bi bi-person-fill" iconSize="lg">
-                        <a href={Routes.PERSONAL_DATA}>Личный кабинет</a>
+                        <a href={Routes.PERSONAL_ACCOUNT}>Личный кабинет</a>
                     </CDBSidebarMenuItem>
                     <CDBSidebarMenuItem className="side-menu-item" icon="bi bi-justify">
                         <a href={Routes.TARIFFS}>Тарифы</a>
