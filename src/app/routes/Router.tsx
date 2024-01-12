b1a5d4d424d1445f7b6cc1e7cd74f5ce6b004447
Vsevolod Romashov <avs.rom@gmail.com>
2024-01-12 22:55:31 +0300

    Resolves #4: Добавлена страница Входа

--- src/app/routes/Router.tsx
+++ src/app/routes/Router.tsx
@@ -1,34 +1,48 @@
 import {memo, useMemo} from 'react';
 import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom';
+
 import {Routes} from './Routes';
 import {Layout} from '../components';
 import {Tariffs} from '../../tariffs';
-import {PersonalAccount} from "../../personalAccount/PersonalAccount";
-import {PersonalData} from "../../personalAccount/personalData";
-import {Tariff} from "../../personalAccount/tariff";
+import {Login} from '../../login';
+import {RequireAuth} from './RequireAuth';
+import {PersonalAccount} from '../../personalAccount/PersonalAccount';
+import {PersonalData} from '../../personalAccount/personalData';
+import {Tariff} from '../../personalAccount/tariff';
 
 export const Router = memo(function Router() {
     const router = useMemo(
         () =>
             createBrowserRouter(
                 createRoutesFromElements(
-                    <Route path={Routes.MAIN_PAGE} element={<Layout/>}>
-                        {/*    Сюда помещаем роуты*/}
+                    <Route>
+                        <Route path={Routes.LOGIN} element={<Login />} />
+                        <Route
+                            path={Routes.MAIN_PAGE}
+                            element={
+                                <RequireAuth>
+                                    <Layout />
+                                </RequireAuth>
+                            }
+                        >
+                            {/*Сюда помещаем роуты*/}
 
-                        {/*Тарифы*/}
-                        <Route path={Routes.TARIFFS} element={<Tariffs/>}/>
+                            {/*Тарифы*/}
+                            <Route path={Routes.TARIFFS} element={<Tariffs />} />
 
-                        {/*Личный кабинет*/}
-                        {/*<Route path={Routes.PERSONAL_ACCOUNT} element={<PersonalAccount/>} />*/}
-                        <Route path={Routes.PERSONAL_ACCOUNT} element={<PersonalAccount/>}>
-                            <Route path={Routes.PERSONAL_DATA} element={<PersonalData/>}/>
-                            <Route path={Routes.TARIFF} element={<Tariff/>}/>
+                            {/*Личный кабинет*/}
+                            {/*<Route path={Routes.PERSONAL_ACCOUNT} element={<PersonalAccount/>} />*/}
+                            <Route path={Routes.PERSONAL_ACCOUNT} element={<PersonalAccount />}>
+                                <Route path={Routes.PERSONAL_DATA} element={<PersonalData />} />
+                                <Route path={Routes.TARIFF} element={<Tariff />} />
+                            </Route>
                         </Route>
+                        ,
                     </Route>,
                 ),
             ),
         [],
     );
 
-    return <RouterProvider router={router}/>;
+    return <RouterProvider router={router} />;
 });
