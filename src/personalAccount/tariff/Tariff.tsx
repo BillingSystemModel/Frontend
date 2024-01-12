05292f6a548dfd97bdeebe1afdd6b50163e8d212
ДшLidiya Klochkova <lida.klochkova.02@mail.ru>
2024-01-12 23:28:25 +0300

    Resolves #16: Create tariff page

--- src/personalAccount/tariff/Tariff.tsx
+++ src/personalAccount/tariff/Tariff.tsx
@@ -1,12 +1,16 @@
 import {memo} from "react";
 import {TariffData} from "./components/tariffData";
 import {TariffDescription} from "./components/tariffDescription";
+import {Button} from "react-bootstrap";
 
-export const Tariff = memo(function Tariff(){
+export const Tariff = memo(function Tariff() {
     return (
         <div className='m-4'>
-            <TariffData />
-            <TariffDescription />
+            <TariffData/>
+            <div className='mt-4'>
+                <TariffDescription/>
+            </div>
+            <Button variant='primary' className='mt-3'>ИЗМЕНИТЬ ТАРИФ</Button>
         </div>
     )
 })
